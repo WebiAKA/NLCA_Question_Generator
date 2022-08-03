a811ab61a3872b560d0aae854f47fbc81c70680e
webby64 <web64td@gmail.com>
2022-08-04 02:35:16 +0900

    [add] summary generator script

--- generate_summaries.py
+++ generate_summaries.py
@@ -10,11 +10,12 @@ openai.api_key = open_ai_api_key
 
 
 seed()
-ctxdir = 'C:/RavenFinetune/contexts/'
-outdir = 'C:/RavenFinetune/summaries/'
-files = os.listdir(ctxdir)
-files = [i for i in files if 'stack' in i]  # filter list: dialog, medical, reddit, stack, news
-files = sample(files, 16)
+dialoguedir = '/Users/webby/Desktop/aka-files/working-dir/NLCA_Question_Generator/dialogues/'
+outdir = '/Users/webby/Desktop/aka-files/working-dir/NLCA_Question_Generator/summaries/'
+promptdir = '/Users/webby/Desktop/aka-files/working-dir/NLCA_Question_Generator/summary_prompt.txt'
+files = os.listdir(dialoguedir)
+# files = [i for i in files if 'stack' in i]  # filter list: dialog, medical, reddit, stack, news
+# files = sample(files, 16)
 print(files)
 
 # davinci-instruct
@@ -27,11 +28,11 @@ print(files)
 def load_prompt(filename, payload):
     with open(filename, 'r', encoding='utf-8') as infile:
         body = infile.read()
-        body = body.replace('<<TEXT>>', payload)
+        body = body.replace('<<DIALOGUE>>', payload)
         return body
 
 
-def completion(prompt, engine='davinci-instruct-beta', temp=0.9, top_p=0.95, tokens=200, freq_pen=0.5, pres_pen=0.5, stop=['asdfasdfasdfasdf']):
+def completion(prompt, engine='davinci-instruct-beta', temp=0.8, top_p=0.95, tokens=200, freq_pen=0.5, pres_pen=0.5, stop=None):
     try:
         response = openai.Completion.create(
             engine=engine,
@@ -48,14 +49,13 @@ def completion(prompt, engine='davinci-instruct-beta', temp=0.9, top_p=0.95, tok
         print('ERROR in completion function:', oops)
 
 for f in files:
-    try:
-        with open(ctxdir + f, 'r', encoding='utf-8') as infile:
-            context = infile.read()
-        prompt = load_prompt('C:/RavenFinetune/p_summary_detailed.txt', context)
-        print('\n---------------------\n', prompt)
-        summary = completion(prompt)
-        print('\n', summary)
-        with open(outdir + f, 'w', encoding='utf-8') as outfile:
-            outfile.write(summary)
-    except Exception as oops:
-        print('ERROR in main loop:', f, oops)
+    
+    with open(dialoguedir + f, 'r', encoding='utf-8') as infile:
+        context = infile.read()
+    prompt = load_prompt(promptdir, context)
+    print('\n---------------------\n', prompt)
+    summary = completion(prompt)
+    print('\n', summary)
+    with open(outdir + f, 'w', encoding='utf-8') as outfile:
+        outfile.write(summary)
+    
